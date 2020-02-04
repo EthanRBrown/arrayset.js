0aea03b2d5e6ea89d33c3f669fae63c3abee087a
Adam Smith <alspdx@gmail.com>
2020-02-04 15:52:41 -0800

    add mutable fromArray method

--- src/arrayset-mutable.js
+++ src/arrayset-mutable.js
@@ -27,6 +27,11 @@ const minus = (a, b, compare) => {
   }
   return a
 }
+const fromArray = a => {
+  for(let i=a.length-1; i>=0; i--) {
+    if(a.slice(0, i).includes(a[i])) a.splice(i, 1)
+  }
+}
 
 const arraysetMutable = a => ({
   contains: x => contains(a, x),
@@ -37,5 +42,7 @@ const arraysetMutable = a => ({
   minus: (b, compare) => minus(a, b, compare),
   difference: (b, compare) => minus(a, b, compare),
 })
+
+arraysetMutable.fromArray = fromArray
 
 module.exports = arraysetMutable
